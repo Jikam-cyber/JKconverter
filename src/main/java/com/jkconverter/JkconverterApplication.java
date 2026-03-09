8058130d4943b0cf8d4e33c3aad5edcb9a80632a
Jonathan Clerger <clergerjonathan807gmail.com>
2026-03-08 20:05:22 -0400

    update render config

--- src/main/java/com/jkconverter/JkconverterApplication.java
+++ src/main/java/com/jkconverter/JkconverterApplication.java
@@ -2,11 +2,12 @@ package com.jkconverter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JkconverterApplication extends SpringBootServletInitializer {
+public class JkconverterApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(JkconverterApplication.class, args);
     }
+
 }
